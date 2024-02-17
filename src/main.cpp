b200591e7e8b29d51514c8429f574b3ca4466fc0
Andrey <kezin636@gmail.com>
2024-02-17 22:14:17 +0300

    looool fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/LevelEditorLayer.hpp>
 #include <Geode/modify/SetGroupIDLayer.hpp>
 #include <array>
 #include <vector>
@@ -33,11 +34,37 @@ int LevelEditorLayer_getNextFreeGroupID(LevelEditorLayer* self, CCArray* p0) {
 	return 0;
 }
 
+class $modify(LevelEditorLayer) {
+	int getNextFreeGroupID(LevelEditorLayer* self, CCArray* p0) {
+		std::set<short> usedGroups = {};
+	
+		CCObject* obj = nullptr;
+
+		CCARRAY_FOREACH(self->m_objects, obj) {
+			GameObject* thisBlock = static_cast<GameObject*>(obj);
+			auto groups = thisBlock->m_groups;
+
+			for (int groupIndex = 0; groupIndex < thisBlock->m_groupCount; groupIndex++) {
+				auto group = groups->at(groupIndex);
+				usedGroups.insert(group);
+			}
+		}
+
+		for (short nextFree = Mod::get()->getSavedValue("relative-next-free", 1); nextFree < 10000; nextFree++) {
+			if (usedGroups.contains(nextFree)) continue;
+
+			return nextFree;
+		}
+
+		return 0;
+	}
+};
+
 $execute {
-	Mod::get()->hook(
+	/*Mod::get()->hook(
 		reinterpret_cast<void*>(geode::base::get() + 0x241530), // address
         &LevelEditorLayer_getNextFreeGroupID, // detour
         "LevelEditorLayer::getNextFreeGroupID", // display name, shows up on the console
         tulip::hook::TulipConvention::Thiscall // calling convention
-	);
+	);*/
 }
